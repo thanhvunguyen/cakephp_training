38c523cdf5a98eb68076b02ebd8deeebae08ffe2
thanhvu <vunt@nal.vn>
2016-11-08 09:32:03 +0700

    [review] review code - User controller - User model

--- app/Controller/UsersController.php
+++ app/Controller/UsersController.php
@@ -3,61 +3,99 @@ App::uses('AppController', 'Controller');
 
 /**
  * Class UsersController
+ *
+ * @property User User
  */
 class UsersController extends AppController {
     var $uses = ['User'];
-    var $helpers = array("Html","Form");
-    var $components = array("Flash");
+
+    var $helpers = [
+        'Html',
+        'Form'
+    ];
+
+    var $components = ['Flash'];
+
+    /**
+     * Function get list .....
+     * from .... tables
+     */
     public function index() {
-        $data = $this->User->find('all');
+        $data = $this->User->getList();
+
         $this->set("data",$data);
     }
-    public function update($id=null){
-        $this->User->set($this->data);
-        if($this->data){
-            if($this->User->validateUser()==TRUE)
-            {
-                $id=$this->data['User']['userid'];;
-                $username = $this->data['User']['name'];;
-                $this->User->updateAll(
-                    array('User.name' => "'$username'"),
-                    array('User.id' => $id)
-                );
-                $this->redirect("index");
-                //$this->Flash->set("Action can be performed !! Fields are in-correct");
-            }
-            else
-            {
-                $this->Flash->set("Action can't be performed !! Fields are in-correct");
 
+    /**
+     * @param null $id
+     * @return bool
+     */
+    public function update($id = null) {
+        if (empty($this->data)) {
+            return false;
+        }
+
+        $this->User->set($this->data);
+
+        if ($this->data) {
+            if ($this->User->validateUser() == true) {
+                $id = $this->data['User']['userId'];
+                $username = $this->data['User']['name'];
+
+                $this->User->updateAll(
+                    ['User.name' => "'$username'"],
+                    ['User.id' => $id]
+                );
+
+                $this->redirect("index");
+            } else {
+                $this->Flash->set("Action can't be performed !! Fields are in-correct");
             }
         }
-        $this->User->id=$id;
+
+        $this->User->id = $id;
         $data = $this->User->findById($id);
+
         $this->set("data",$data);
     }
-    public function create(){
+
+    /**
+     *
+     */
+    public function create() {
+        if (empty($this->data)) {
+            return false;
+        }
 
         $this->User->set($this->data);
-        if($this->data){
-            if($this->User->validateUser()==TRUE)
-            {
+        if ($this->data) {
+            if ($this->User->validateUser() == true) {
                 $this->User->save($this->data);
+
                 $this->redirect("index");
-            }
-            else
-            {
+            } else {
                 $this->Flash->set("Action can't be performed  !! Fields are in-correct");
             }
         }
-
     }
+
+    /** TODO: now don't need */
     public function info(){
 
     }
-    public function delete($id=null){
-        $this->User->id=$id;
+
+    /**
+     * @param null $id
+     */
+    public function delete ($id = null) {
+
+        /** TODO: validate id (integer, exist in DB) */
+
+        $this->User->id = $id;
         $this->User->deleteAll(array('id'=>$id));
+
+        // Message bao thanh cong sau khi xoa
+
         $this->redirect("index");
     }
 
--- app/Model/User.php
+++ app/Model/User.php
@@ -1,60 +1,62 @@
 <?php
+
 /**
- * Created by PhpStorm.
- * User: Win_64
- * Date: 11/3/2016
- * Time: 2:14 PM
+ * Class User
+ *
+ * description
+ * validate data from users table
+ *
  */
 class User extends AppModel {
-    var $name = "User";
-    public $validate = array();
-    function validateUser()
-    {
-        $this->validate = array(
-            "name" => array(
-                "rule1" => array(
-                    "rule" => "notBlank",
-                    "message" => "Name not empty !",
-                ),
-                "rule2" => array(
-                    "rule"=> array('lengthBetween', 6, 10),
-                    "message" => "Name must be between 5 and 15 characters long."
-                ),
-                "rule3" => array(
-                    "rule"=>"isUnique",
-                    "message" => "Your Username have already existed "
-                ),
-            ),
-            "phone" => array(
-                "rule1" => array(
-                    "rule" => "notBlank",
-                    "message" => "Phone not empty !",
-                ),
-                "rule2" => array(
-                    "rule"=> array('lengthBetween', 10, 11),
-                    "message" => "Phone must be between 10 and 11 characters long."
-                ),
-                "rule3" => array(
-                    "rule"=>"Numeric",
-                    "message" => "Phone number should be numeric"
-                ),
-            ),
-            "email" => array(
-                "rule1" => array(
-                    "rule" => "notBlank",
-                    "message" => "Email not empty !",
-                ),
 
-                "rule2" => array(
-                    "rule"=>"email",
-                    "message" => "Please enter a valid email address"
-                ),
+    var $name = "User";
+
+    public $validate = array("name" => array(
+        "rule1" => array(
+            "rule" => "notBlank",
+            "message" => "Name not empty !",
+        ),
+        "rule2" => array(
+            "rule"=> array('lengthBetween', 6, 10),
+            "message" => "Name must be between 5 and 15 characters long."
+        ),
+        "rule3" => array(
+            "rule"=>"isUnique",
+            "message" => "Your Username have already existed "
+        ),
+    ),
+        "phone" => array(
+            "rule1" => array(
+                "rule" => "notBlank",
+                "message" => "Phone not empty !",
             ),
-            "iduser" => array("rule" => "notBlank", "message" => "id not empty !"));
-        if($this->validates($this->validate))
-            return TRUE;
-        else
-            return FALSE;
+            "rule2" => array(
+                "rule"=> array('lengthBetween', 10, 11),
+                "message" => "Phone must be between 10 and 11 characters long."
+            ),
+            "rule3" => array(
+                "rule"=>"Numeric",
+                "message" => "Phone number should be numeric"
+            ),
+        ),
+        "email" => array(
+            "rule1" => array(
+                "rule" => "notBlank",
+                "message" => "Email not empty !",
+            ),
+
+            "rule2" => array(
+                "rule"=>"email",
+                "message" => "Please enter a valid email address"
+            ),
+        ),
+        "iduser" => array("rule" => "notBlank", "message" => "id not empty !"));
+
+    /**
+     * @return array|null
+     */
+    public function getList() {
+        return $this->find('all');
     }
 
 }
